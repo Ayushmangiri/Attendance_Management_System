6ef2fabd5c90c60890118f47a09726fd094daac5
Ayushmangiri <ayushmangiri@gmail.com>
2025-08-03 11:50:24 +0530

    Added Feature

--- src/test/java/com/Attendance/Attendance_Management_System/model/Attendance.java
+++ src/test/java/com/Attendance/Attendance_Management_System/model/Attendance.java
@@ -1,4 +1,28 @@
 package com.Attendance.Attendance_Management_System.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+@Data
+@Generated
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
 public class Attendance {
+    @Id  //Ye primary key hoga (unique ID har student ka)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    Auto-increment karega ID ko DB ke through
+    private Long id;
+    private LocalDate date;
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
 }
--- src/test/java/com/Attendance/Attendance_Management_System/model/Student.java
+++ src/test/java/com/Attendance/Attendance_Management_System/model/Student.java
@@ -1,4 +1,61 @@
 package com.Attendance.Attendance_Management_System.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity  // Ye class ab DB table me convert hogi
 public class Student {
+
+    @Id  // Primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // Auto-increment
+    private Long id;
+
+    private String name;
+    private Integer rollNo;
+    private String subject;
+    private String className;
+
+    // Getter and Setter Methods — ye help karte hain value set/get karne me
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(Integer rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
 }
